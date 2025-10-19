3693c3f2c4187b09381f30a27bb4cfb189eeb7ef
GyoryLaszlo <laszlo.gyory@gmail.com>
2025-10-19 07:56:52 +0200

    starter default code in java

---
+++ src/main/java/com/example/gyakbeadando/GyakBeadandoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gyakbeadando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyakBeadandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GyakBeadandoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gyakbeadando/GyakBeadandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gyakbeadando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyakBeadandoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
