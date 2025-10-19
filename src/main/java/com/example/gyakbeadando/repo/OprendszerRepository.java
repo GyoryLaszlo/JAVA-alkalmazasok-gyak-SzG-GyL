63c5fedd3569523d382f6b9809d4c34339354799
GyoryLaszlo <laszlo.gyory@gmail.com>
2025-10-19 08:23:30 +0200

    Repositories

---
+++ src/main/java/com/example/gyakbeadando/repo/OprendszerRepository.java
@@ -0,0 +1,6 @@
+package com.example.gyakbeadando.repo;
+
+import com.example.gyakbeadando.model.Oprendszer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OprendszerRepository extends JpaRepository<Oprendszer, Long> {}
---
+++ src/main/java/com/example/gyakbeadando/repo/ProcesszorRepository.java
@@ -0,0 +1,6 @@
+package com.example.gyakbeadando.repo;
+
+import com.example.gyakbeadando.model.Processzor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProcesszorRepository extends JpaRepository<Processzor, Long> {}
